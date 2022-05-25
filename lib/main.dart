5f4dadec5538ffa99c86631d076852fcd8d03ce9
yehyaajour <yehyamofed@gmail.com>
2022-05-25 11:21:20 +0300

    testDeploy

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-//test
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
